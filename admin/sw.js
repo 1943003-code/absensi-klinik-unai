f1a2f5c80ea5c594f27da001376a3f57b2d53e22
1943003-code <1943003@unai.edu>
2026-09-05 01:12:30 +0700

    Add files via upload

---
+++ admin/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME='admin-absensi-unai-v1';
+const APP_SHELL=['/admin/','/admin/index.html','/admin/manifest.webmanifest','/icon-192.png','/icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k.startsWith('admin-absensi-unai-') && k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(r => r || caches.match('/admin/index.html')))
+  );
+});
