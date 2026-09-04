aa5dec39f45b7f5bd6572fd21d24ee4cb50a1b53
1943003-code <1943003@unai.edu>
2026-09-05 04:49:20 +0700

    Update sw.js

--- admin/sw.js
+++ admin/sw.js
@@ -1,29 +1,52 @@
-const CACHE_NAME='admin-absensi-unai-v3';
-const APP_SHELL=['/admin/','/admin/index.html','/admin/manifest.webmanifest','/icon-192.png','/icon-512.png'];
+const CACHE_NAME = 'admin-absensi-unai-v4';
+
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k.startsWith('admin-absensi-unai-') && k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k.startsWith('admin-absensi-unai-') && k !== CACHE_NAME)
+          .map(k => caches.delete(k))
+      )
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+
   event.respondWith(
     fetch(event.request)
       .then(response => {
         const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+
+        if (new URL(event.request.url).origin === self.location.origin) {
+          caches.open(CACHE_NAME)
+            .then(cache => cache.put(event.request, copy))
+            .catch(() => {});
+        }
+
         return response;
       })
-      .catch(() => caches.match(event.request).then(r => r || caches.match('/admin/index.html')))
+      .catch(() =>
+        caches.match(event.request)
+          .then(r => r || caches.match('./index.html'))
+      )
   );
 });
