822f6533c31965c04e3e7bbdcb804a04c391e7c0
1943003-code <1943003@unai.edu>
2026-09-05 02:43:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://ikyppjffwcbiwvhjtaxi.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_bS3PIfkN___vwz2qXdflzw_UkN7O7MG",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlreXBwamZmd2NiaXd2aGp0YXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2MDAxMzIsImV4cCI6MjEwMzE3NjEzMn0.DiVpdV-ZjjXGoFUff2QryatNkbvRHMQVYfsSdY66_Yk",
   APP_NAME: "Absensi UNAI"
 };
