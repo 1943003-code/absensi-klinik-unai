085904530d99cdd306f59539060c56421c731140
1943003-code <1943003@unai.edu>
2026-09-05 02:57:48 +0700

    Update sw.js

--- admin/sw.js
+++ admin/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='admin-absensi-unai-v1';
+const CACHE_NAME='admin-absensi-unai-v3';
 const APP_SHELL=['/admin/','/admin/index.html','/admin/manifest.webmanifest','/icon-192.png','/icon-512.png'];
 
 self.addEventListener('install', event => {
