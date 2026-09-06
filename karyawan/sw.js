c9415bf519800867904c1c01e6369901d36fdbf3
1943003-code <1943003@unai.edu>
2026-09-06 21:21:19 +0700

    Retire legacy employee PWA cache and force redirect

--- karyawan/sw.js
+++ karyawan/sw.js
@@ -1,48 +1,20 @@
-const CACHE_NAME = 'absensi-karyawan-unai-v4';
-
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(k => k.startsWith('absensi-karyawan-unai-') && k !== CACHE_NAME)
-          .map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k.startsWith('absensi-karyawan-unai-')).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME)
-          .then(cache => cache.put(event.request, copy))
-          .catch(() => {});
-        return response;
-      })
-      .catch(() =>
-        caches.match(event.request)
-          .then(r => r || caches.match('./index.html'))
-      )
-  );
+  if (event.request.mode === 'navigate') {
+    const url = new URL(event.request.url);
+    if (url.pathname.startsWith('/absensi-klinik-unai/karyawan')) {
+      event.respondWith(Response.redirect('/absensi-klinik-unai/', 302));
+    }
+  }
 });
